8da2165c9a32f26892ab9434d396087c84d7e836
Valpip123EMY <valmik.nahata@gmail.com>
2026-01-23 15:38:59 -0800

    Fix next config and build process for clean export

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+    /* config options here */
+    reactStrictMode: true,
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
